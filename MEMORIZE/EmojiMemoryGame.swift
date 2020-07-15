c94d1eef7f552c3efcfddb655994ed64b8192d70
SETH VERNON <sethvernon@icloud.com>
2020-07-14 20:07:07 -0500

    first commit

---
+++ MEMORIZE/EmojiMemoryGame.swift
@@ -0,0 +1,9 @@
+//
+//  EmojiMemoryGame.swift
+//  MEMORIZE
+//
+//  Created by MacBookPro on 7/11/20.
+//  Copyright © 2020 SETH VERNON. All rights reserved.
+//
+
+import Foundation
---
+++ MEMORIZE/MemoryGame.swift
@@ -0,0 +1,9 @@
+//
+//  MemoryGame.swift
+//  MEMORIZE
+//
+//  Created by MacBookPro on 7/11/20.
+//  Copyright © 2020 SETH VERNON. All rights reserved.
+//
+
+import Foundation
